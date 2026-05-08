2fbacd2b51c61c5cfdde0a89a6f559b83805132a
Destiny Tch <182261473+DestinyTch@users.noreply.github.com>
2026-05-08 14:05:52 +0100

    Update application run configuration for port

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def create_app() -> Flask:
 
 if __name__ == "__main__":
     application = create_app()
-    application.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host="0.0.0.0", port=port, debug=True)
